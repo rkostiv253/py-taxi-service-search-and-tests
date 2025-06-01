eb78c2860d2d69143bcc6c0ec7e71a493296b9a8
Roman Kostiv <rkostiv@gmail.com>
2025-06-01 18:00:02 +0300

    Solution

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,6 +3,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 from taxi.forms import ManufacturerSearchForm
+from taxi.models import Manufacturer
 
 
 class SearchTest(TestCase):
@@ -11,3 +12,14 @@ class SearchTest(TestCase):
         response = client.get(reverse("taxi:driver-list"))
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.context["search_form"], ManufacturerSearchForm)
+
+    def test_search_manufacturer_filtering(self):
+        Manufacturer.objects.create(name="BMW", country="Germany")
+        Manufacturer.objects.create(name="Ferrari", country="Italy")
+
+        client = Client()
+        response = client.get(reverse("taxi:driver-list"), {"name": "BMW"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "BMW")
+        self.assertNotContains(response, "Ferrari")
